b7f97b818d824cfaf2cb99f8caa322d2dd34f5fd
shfkahariff <shafikahariffin@gmail.com>
2024-01-26 18:01:01 +0800

    init commit

---
+++ app/javascript/application.js
@@ -0,0 +1,16 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import "./add_jquery"
+import 'turn.js'
+import './lib/turn.min.js'
+import './lib/turn.js'
+import './lib/hash.js'
+import './lib/scissor.js'
+import './lib/scissor.min.js'
+import './lib/turn.html4.js'
+import './lib/turn.html4.min.js'
+import './lib/zoom.js'
+import './lib/zoom.min.js'
+
+
